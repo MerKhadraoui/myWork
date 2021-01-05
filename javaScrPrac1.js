54fdab170fa59c8adaddf504ae1fdcb4c1abc570
MerKhadraoui <75792175+MerKhadraoui@users.noreply.github.com>
2021-01-05 18:25:17 +0100

    04-01-2021

--- javaScrPrac1.js
+++ javaScrPrac1.js
@@ -1,44 +1,4 @@
-// printing out
-console.log("Miya");
-let firstName= "meriem";
-console.log(firstName.length);
 
-let somthing=32423;
-somthing=4576;
-console.log(somthing);
-
-// constant variable.
-const fatherName="Lkhamissi khadraoui";
-console.log(fatherName);
-// var ist a global variabl
-
-var global="i am meriem";
-// function functionname (parametr);{}
-
-function print(str){
-console.log(str);
-
-}
-print("miyakhad");
-print(1997);
- let oString="hi My Name is Meriem, I'm 33 year.";
-print(oString.length);
-let str1 =oString.substring(5);
-console.log(str1);
-let str2= oString.substring(6, 17);
-console.log(str2);
-//
-let str3=oString.toUpperCase();
-console.log(str3);
-let str4=oString.toLowerCase();
-console.log(str4);
-let nam="MeRieM";
-let Fnam=nam[0];
-console.log(Fnam);
-let Res=nam.substring(1).toLowerCase();
-console.log(Res);
-let fiNam= Fnam+Res;
-console.log(fiNam);
 //Exercises 
 //1.Declare a variable with the value of “I can walk in the park all day!“. Print the word “park” to the console.
 let sant="I can walk in the park all day!";
