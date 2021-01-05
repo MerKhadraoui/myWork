920dfaae5dccfbbddce13445ab2f2de11c4ab3cf
MerKhadraoui <75792175+MerKhadraoui@users.noreply.github.com>
2021-01-05 18:17:11 +0100

    Create javaExercise.js
    05-01

---
+++ javaExercise.js
@@ -0,0 +1,48 @@
+
+var x;
+x= "";
+console.log(typeof x); 
+///
+true ? console.log("cool"):console.log("not cool");
+let bool = 0;
+//
+let str="meriemrrr";
+console.log(str[str.length-1]);
+// trim()
+
+let longStr="Hi itas memeriem           ";
+console.log(longStr.trim());
+
+function display (str){
+
+    console.log(str);
+
+//1. Create two variables.
+// One variable should contain a string value and
+//the other should contain a number. Concatenate the string and the number.
+
+
+let strValue="merykhad";
+
+let numValue= 1987;
+longStr1=strValue+numValue;
+console.log(longStr1);
+
+//2. Check if the following numbers are even numbers. Use a ternary and if the number is even print 30 is even, else print that it is odd.
+
+
+
+//exersice 2
+
+
+// Create a variable with the value of “123”. Convert it to a number.
+let varValue="123"; 
+console.log(parseInt(varValue,10));
+
+// Create a variable with the value of “130.7". Convert it to a number.
+let VarValue2= "130.7";
+console.log(parseFloat(VarValue2));
+// Declare a variable named isDog. If true, print ‘pat, pat’ and if not, print ‘find me a dog to pat!’
+let isDog="";
+
+isDog== true?console.log("pat,pat"):console.log("find me a dog to pat!");
