7e10de417aacbf1029f79a7ebff3e52adc022954
meriemkhadraoui <miyakhad@gmail.com>
2021-01-07 14:38:29 +0100

    Exersice07-01

--- Jan/05-01/exersice3.js
+++ Jan/05-01/exersice3.js
@@ -18,6 +18,7 @@ console.log(Math.max(-1, 0, 1, 2, 3, 4));
 console.log(Math.floor(3321.32321), Math.floor(326.76), Math.floor(76788.7), Math.floor(-9.78),Math.floor(43.342) );
 
 
+
 //b. Round down
 //Round down each of the following numbers to the nearest integer: 3321.32321, 326.76, 76788.7, -9.78, 28.329.
 console.log(Math.ceil(3321.32321), Math.ceil(326.76), Math.ceil(76788.7), Math.ceil(-9.78),Math.ceil(43.342) );
---
+++ Jan/07-01/index07-01.js
@@ -0,0 +1,21 @@
+
+ // Exersice1
+ //1Create a program that prints a random integer from 1 - 6.
+console.log(Math.floor(Math.random()*6)+1);
+
+
+
+
+
+
+////the area of A circle   formula A= p * r²
+r = 5;
+function calAreaCircle(monP) {
+    return A =  Math.PI * (Math.pow(r, 2));
+  }
+  
+  console.log(Math.PI);
+  
+  console.log(calAreaCircle(r));
+  console.log("the area of A circle is :" + (calAreaCircle(r).toFixed(2))+" m²");
+
