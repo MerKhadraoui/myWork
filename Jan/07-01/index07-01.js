3f133c29fbbc94af319850c603986cfc4c6606c5
meriemkhadraoui <miyakhad@gmail.com>
2021-01-11 16:05:54 +0100

    new work day

--- Jan/07-01/index07-01.js
+++ Jan/07-01/index07-01.js
@@ -19,3 +19,61 @@ function calAreaCircle(monP) {
   console.log(calAreaCircle(r));
   console.log("the area of A circle is :" + (calAreaCircle(r).toFixed(2))+" m²");
 
+
+ /* 1 Task
+Create a function that takes the age and return the age in days.
+calcAge(65) ➞ 23725
+calcAge(0) ➞ 0
+calcAge(20) ➞ 7300
+Notes
+Use 365 days as the length of a year for this challenge.
+Ignore leap years and days between last birthday and now.
+Expect only positive integer inputs.*/
+
+
+function calcAge(age){
+let d=365;
+let ld=366;
+let don2=(age/4)-1;
+ let normelYear=don2 *d ;
+let leapYear=(age-don2)*ld;
+return resultAgeD=normelYear+leapYear;
+
+}   
+
+console.log("your Age in Days is : "+ calcAge(60));
+
+/*Create a function that takes the number of wins, draws and losses and calculates the number of points a football team has obtained so far.
+wins get 3 points
+draws get 1 point
+losses get 0 points*/
+
+function calcPoints(wins,draws,losses){
+let pointW=wins*3;
+
+return sumPoints=pointW+draws;
+
+}
+
+console.log("Sum of your points is: "+ calcPoints(3,2,4));
+console.log(calcPoints(3,2,4)>=15?console.log("you are qualified"):console.log("you are not qualified"));
+
+
+
+/*A word is on the loose and now has tried to hide amongst a crowd of tall letters! Help write a function to detect what the word is, knowing the following rules:
+The wanted word is in lowercase.
+The crowd of letters is all in uppercase.
+Note that the word will be spread out amongst the random letters, but their letters remain in the same order.
+detectWord("UcUNFYGaFYFYGtNUH") ➞ "cat"
+detectWord("bEEFGBuFBRrHgUHlNFYaYr") ➞ "burglar"
+detectWord("YFemHUFBbezFBYzFBYLleGBYEFGBMENTment") ➞ "embezzlement"*/
+
+
+function  wordLowercas(recherche){
+
+return text = recherche.includes("cat");
+
+
+}
+
+console.log(wordLowercas("FBMGFKGcGJGNaGBKMt"));
---
+++ Jan/11-01/index.js
@@ -0,0 +1,187 @@
+
+// old school Function
+function print(str) {
+    console.log(str);
+}
+// The new form of create a function Es 6 const display=(str)=>{ console.log(str);};
+
+const display = (str) => {
+    console.log(str);
+
+};
+print("Hi");
+display("Meriem");
+const sum = (y, x) => x + y;
+console.log(sum(4, 6)); const isBigger = (num1, num2) => num1 < num2;
+console.log(isBigger(33, 87));
+const calculatSum = (x1, y1) => {
+    let v = 234;
+    return x1 + y1 * v;
+};
+const printFullWord = (firstName, age, add) => console.log(`Hi ${firstName}, I am ${age},I live is${add}`)
+printFullWord("meriemkhadraoui", 33, "Hamburg");
+
+let isLogged = false;
+const login = () => { isLogged = true; };
+login();
+let name1 = "Meriem";
+let name2 = "miya";
+let name3 = "toma";
+let namesArray = [name1, name2, name3];
+console.log(namesArray[0]);
+let numrArr = [0, 1, 5, 3, 9, 8, 7, 2, 5];
+console.log(numrArr[6]);
+let myHobs = ["cooking", "football", "dance", "chess"];
+//
+for (let i = 0; i < myHobs.length; ++i) {
+    console.log(myHobs[i]);
+}
+for (let i = 10; i >= 0; i--) {
+    console.log(`1 * ${i} = ${i * 1}`);
+}
+const charArr = ["amina", "boubi", "cathia"];
+for (let i = 0; i < charArr.length; i++) {
+    console.log(charArr[i][0].toUpperCase());
+}
+
+const namesArr = ["meirem", "khadraoui"];
+for (let i = 0; i < namesArr.length; i++) {
+    console.log(namesArr[i][0].toUpperCase() + namesArr[i].slice(0));
+}
+let firstA = true;
+let firstB = false;
+let firstC = 11 == 23;
+if ((firstA == firstB && firstC != firstA) && firstA == firstB) {
+    console.log("wow");
+
+}
+else {
+    console.log("I don't ");
+
+}
+//Write a program that will iterate from 0 to 20. For each iteration, 
+//it will check if the current number is even or odd, and report that to the screen (e.g. “2 is even”).
+
+
+for (let i = 0; i <= 20; i++) {
+    if (i % 2 == 0) {
+        console.log(`${i} is even`);
+    }
+    else {
+        console.log(`${i} is odd`);
+    }
+
+}
+
+/*     Write programs that produce the following outputs:
+100 200 300 400 500 600 700 800 900 1000
+*/
+text = " ";
+for (i = 0; i <= 10; i++) {
+    text += i * 100 + " ";
+
+}
+
+console.log(text);
+
+//0 2 4 6 8 10
+text = " ";
+for (i = 0; i <= 5; i++) {
+    text += 2 * i + " ";
+    if (i % 2 == 0) {
+
+    }
+
+
+}
+console.log(text);
+
+
+//3 6 9 12 15
+text = " ";
+for (i = 0; i <= 15; i+=3) {
+    text += i + " ";
+    if (i % 2 !== 0) {
+
+    }
+
+
+}
+console.log(text);
+//9 8 7 6 5 4 3 2 1 0
+text="";
+for (let i = 9; i >= 0; i--){
+text+=i+" ";
+}
+console.log(text)
+
+
+    //1 1 1 2 2 2 3 3 3 4 4 4
+    text = " ";
+countNum = 0;
+for (let i = 1; i <= 4;) {
+    text += i + " ";
+    if (countNum != 0 && countNum % 3 == 0) {
+        i++;
+        countNum = 0;
+
+    }
+    countNum++;
+
+
+}
+console.log(text);
+
+
+
+let strToArra = "Hi this is me";
+let strToArra1 = strToArra.split(" ");
+console.log(strToArra1);
+let cackTo = strToArra1.join(" ");
+console.log(cackTo);
+console.log("Hi".repeat(4));
+
+
+
+//0 1 2 3 4 0 1 2 3 4 0 1 2 3 4   ||
+
+text = " ";
+countNum = 0;
+  for (let i = 0; i <= 4;) {
+    text += countNum + " "; 
+    if (countNum !=0 && countNum % 4 == 0) {
+     i++;
+
+        countNum = 0;
+ 
+    }
+   countNum++;
+
+}
+console.log(text);
+
+
+
+const countOccurrence=(sTr,cHar)=>{
+    let strToAr=sTr.split(" ");
+    let countT=0;
+    for(let i=0; i<strToAr.length;i++){
+        if(strToAr[i]=cHar){
+            countT++;}
+       countOccurrence("bfggbfgNHJUIFGb","g")
+ console.log(countT);
+}  
+  }
+   
+
+
+
+
+
+
+
+
+
+
+
+
